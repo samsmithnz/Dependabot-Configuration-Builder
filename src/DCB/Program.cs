b62ba9ad564d0827c6c03f4a1ba919a2c724bf84
Sam Smith <samsmithnz@gmail.com>
2021-12-11 20:59:41 -0500

    Getting all files

--- src/DCB/Program.cs
+++ src/DCB/Program.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace DCB
+﻿namespace DCB
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string startingDirectory = @"C:\Users\samsm\source\repos\Dependabot-Configuration-Builder";
+            string fileToSearch = "*.csproj";
+            List<string> files = GetFilesForDirectory(startingDirectory, fileToSearch);
+            Console.WriteLine(files.Count + " files found");
+        }
+
+        public static List<string> GetFilesForDirectory(string startingDirectory, string fileToSearch)
+        {
+            string[] files = Directory.GetFiles(startingDirectory, fileToSearch, SearchOption.AllDirectories);
+            return files.ToList<string>();
         }
     }
 }
