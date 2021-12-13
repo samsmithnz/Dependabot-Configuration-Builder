a0e6e21888aa23218f8a8864fda610d086b6bf23
Sam Smith <samsmithnz@gmail.com>
2021-12-13 16:26:06 +0000

    Fixed path to use current path

--- src/DCB/Program.cs
+++ src/DCB/Program.cs
@@ -5,7 +5,6 @@
         public static void Main(string[] args)
         {
             string workingDirectory = Environment.CurrentDirectory;
-            //string? projectDirectory = Directory.GetParent(workingDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName;
             List<string> files = FileSearch.GetFilesForDirectory(workingDirectory);
             Console.WriteLine(files.Count + " files found in " + workingDirectory);
 
